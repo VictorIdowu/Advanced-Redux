e669fe9d2dbcfa5a283cfd6ae9c7fbae8b8f0e08
VictorIdowu <idsonvictor@gmail.com>
2024-01-20 14:59:59 +0100

    done

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,24 @@
+import { useDispatch } from "react-redux";
 import classes from "./CartItem.module.css";
+import { cartActions } from "../store/cart-slice";
 
 const CartItem = ({ item }) => {
-  const { title, quantity, total, price } = item;
+  const { title, quantity, total, price, id } = item;
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +34,8 @@ const CartItem = ({ item }) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
         </div>
       </div>
     </li>
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,35 @@
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const PRODUCTS = [
+  {
+    id: "p1",
+    price: 6,
+    title: "My First book",
+    description: "The first book I ever wrote",
+  },
+  {
+    id: "p2",
+    price: 8,
+    title: "My Second book",
+    description: "The second book I ever wrote",
+  },
+];
+
 const Products = () => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title="Test"
-          price={6}
-          description="This is a first product - amazing!"
-        />
+        {PRODUCTS.map((prod) => (
+          <ProductItem
+            key={prod.id}
+            id={prod.id}
+            title={prod.title}
+            price={prod.price}
+            description={prod.description}
+          />
+        ))}
       </ul>
     </section>
   );
