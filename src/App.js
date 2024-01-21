647fd5ac38ce8c6be6bdf25cbae698439594be77
VictorIdowu <idsonvictor@gmail.com>
2024-01-21 16:26:46 +0100

    UseEffect

--- src/App.js
+++ src/App.js
@@ -2,9 +2,27 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    const postData = async () => {
+      const res = await fetch(
+        "https://redux-practice-d5c4d-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      console.log(await res.json());
+    };
+
+    postData();
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
